44bd42e954e450fd0c5cd6cb74006e3f6c7f0eab
Leonardo Lopes <leonardolopes415@gmail.com>
2020-05-04 14:47:49 -0300

    Mult implementado

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,22 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@0
+D = A
+@2
+M = D
+@0
+D = M
+@16
+D, JEQ
+@1
+D = M
+@2
+M = D + M
+@0
+M = M - 1
+@4
+0,JMP
+
+
+
